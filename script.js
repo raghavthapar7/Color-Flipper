71319d0867a598c6f5958efe99740c0e863e84cf
Raghav Thapar <raghavthapar.apps@gmail.com>
2023-08-18 18:17:42 +0530

    Removed the box, centered the page

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ let generateRandomColor = function () {
 };
 
 let colorButton = document.getElementsByClassName("btn-change")[0];
-let colorBox = document.getElementById("box");
+// let colorBox = document.getElementById("box");
 
 colorButton.addEventListener("click", function () {
   let r = generateRandomColor();
   let g = generateRandomColor();
   let b = generateRandomColor();
 
-  colorBox.style.backgroundColor = `rgb(${r},${g},${b})`;
+  // colorBox.style.backgroundColor = `rgb(${r},${g},${b})`;
   console.log(r, g, b);
 });
