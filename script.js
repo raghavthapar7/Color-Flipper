caa5aeaf67aab8d8b67fb66fe69019ae151653c6
Raghav Thapar <raghavthapar.apps@gmail.com>
2023-08-18 17:43:48 +0530

    Random number generating logic addec

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 "use strict";
 
+let generateRandomColor = function () {
+  return Math.round(Math.random() * 255);
+};
+
 let colorButton = document.getElementsByClassName("btn-change")[0];
 
 colorButton.addEventListener("click", function () {
-  alert("Hi");
+  console.log(generateRandomColor());
 });
