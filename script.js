312af04d0b82d424d4cb3763ea56d9c2282f28d2
Raghav Thapar <raghavthapar.apps@gmail.com>
2023-08-18 18:56:53 +0530

    Removed the change button and made some aesthetic changes

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ let generateRandomColor = function () {
   return Math.round(Math.random() * 255);
 };
 
-let colorButton = document.getElementsByClassName("btn-change")[0];
+// let colorButton = document.getElementsByClassName("btn-change")[0];
 let header = document.getElementsByClassName("header")[0];
 let color = document.getElementsByClassName("color")[0];
 // let colorBox = document.getElementById("box");
 
-colorButton.addEventListener("click", function () {
+header.addEventListener("click", function () {
   let r = generateRandomColor();
   let g = generateRandomColor();
   let b = generateRandomColor();
 
-  header.style.backgroundColor = `rgb(${r},${g},${b})`;
-  color.textContent = `rgb(${r},${g},${b})`;
+  header.style.backgroundColor = `RGB(${r},${g},${b})`;
+  color.textContent = `RGB(${r},${g},${b})`;
   console.log(r, g, b);
 });
